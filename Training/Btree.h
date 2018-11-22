ec8956e8066e2ae64ab6e6a1a5425cedb19c7ac8
Yigao Fan <v-yifan@microsoft.com>
2018-11-22 19:10:58 +0800

    Code the find and insert method a little.

--- Training/Btree.h
+++ Training/Btree.h
@@ -1,22 +1,1 @@
-#pragma once
-#include <iostream>
-
-struct Node {
-	Node* _nextBrother;
-	size_t childCount;
-};
-class Btree {
-	friend std::ostream& operator<<(std::ostream& os, const Btree& item);
-private:
-	Node root = {
-		nullptr,
-		1,
-	};
-
-public:
-	Btree();
-	~Btree();
-};
-
-
-
+#pragma once #include <iostream> #include <vector> using std::vector; typedef int ValueType; struct Node; typedef bool(*predicateFunc)(const Node);  struct Node { 	Node* _1stChild; 	ValueType _2ndLowBound; 	Node* _2ndChild; 	ValueType _3rdLowBound; 	Node* _3rdChild;  	size_t childCount; 	Node* _nextBrother; };  class Btree { 	friend std::ostream& operator<<(std::ostream& os, const Btree& item); private: 	Node root = { 		nullptr, 		1, 	}; 	void adjust();  public: 	Btree(); 	~Btree(); 	Btree(ValueType e1, ValueType e2, ...); 	vector<Node&> find(predicateFunc func); 	int insert(ValueType e); };    
