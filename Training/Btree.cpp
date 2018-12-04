ddd4224ef4e624a00d9da44524c3e305dc2d1d80
YigaoFan <fanyigao@qq.com>
2018-12-05 02:41:05 +0800

    update a little format

--- Training/Btree.cpp
+++ Training/Btree.cpp
@@ -24,18 +24,18 @@ Btree::~Btree()
 
 ostream& operator<<(ostream& os, const Btree& item)
 {
-    //os << item.root.childCount << endl;
-    return os;
+	//os << item.root.childCount << endl;
+	return os;
 }
 
 // this is for insert method, maybe not suit for normal search
 shared_ptr<Node> Btree::fit(PredicateFunc func)
 {
-    // for-each Node to chose which should be continued
+	// for-each Node to chose which should be continued
 	shared_ptr<Node> currentNode;
-    for (currentNode = root;
-    currentNode->hasChild(); // here ensure not explore the leaf
-    currentNode = currentNode->giveMeTheWay(func));
+	for (currentNode = root;
+			currentNode->hasChild(); // here ensure not explore the leaf
+			currentNode = currentNode->giveMeTheWay(func));
 	// will get the leaf node
 	return currentNode;
 }
@@ -43,22 +43,20 @@ shared_ptr<Node> Btree::fit(PredicateFunc func)
 int Btree::insert(KeyType e)
 {
 	std::cout << "insert key: " << e << endl;
-    // expression below exist some problem need to be clear
-    // the return value is the father?
-    // using Currying to determine a argument
-    shared_ptr<Node> parent = fit([e] (const Ele& ele) {
-	    if (ele.childValueLowBound > e) {
+	// expression below exist some problem need to be clear
+	// the return value is the father?
+	// using Currying to determine a argument
+	shared_ptr<Node> parent = fit([e] (const Ele& ele) {
+			if (ele.childValueLowBound > e) {
 			return true;
-	    }
-		else {
+			} else {
 			return false;
-		}
-	});
+			}});
 
 	//todo: here I use the std::sort directly,
 	//todo: in the future, update to use insert sort
 	auto& eles = parent->getVectorOfEles();
-	
+
 	std::cout << "original: ";
 	for (auto& leaf : eles) {
 		std::cout << leaf.key << " ";
@@ -67,15 +65,15 @@ int Btree::insert(KeyType e)
 
 	eles.push_back(e);
 	std::sort(eles.begin(), eles.end(),
-		[](const Ele& ele1, const Ele& ele2)
-	{
-		if (ele1.key < ele2.key) {
+			[](const Ele& ele1, const Ele& ele2)
+			{
+			if (ele1.key < ele2.key) {
 			return true;
-		} else {
+			} else {
 			return false;
-		}
-	});
-	
+			}
+			});
+
 	std::cout << "new: ";
 	for (auto& leaf : eles) {
 		std::cout << leaf.key << " ";
@@ -86,5 +84,5 @@ int Btree::insert(KeyType e)
 
 void Btree::adjust()
 {
-	
+
 }
