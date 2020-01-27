e3918902173d428c4ce490ff20e14ec58f80e9d9
unknown <fanyigao@qq.com>
2020-01-27 21:36:11 +0800

    complete LeafNode::Remove

--- src/Btree/LeafNode.hpp
+++ src/Btree/LeafNode.hpp
@@ -10,9 +10,26 @@ namespace Collections
 	using ::std::move;
 	using ::std::make_unique;
 
+	namespace
+	{
+		enum Position
+		{
+			Previous,
+			Next,
+		};
+
+		// Why this enum can not define in LeafNode class? because it has LeafNode::Add adn LeafNode::Remove
+		enum Operation
+		{
+			Add,
+			Remove,
+		};
+	}
+
 #define LEAF LeafNode<Key, Value, BtreeOrder>
 	template <typename Key, typename Value, order_int BtreeOrder>
 	class LeafNode : public NodeBase<Key, Value, BtreeOrder>
+#undef LEAF
 	{
 	private:
 		using _LessThan = LessThan<Key>;
@@ -21,7 +38,8 @@ namespace Collections
 		LeafNode* _next{ nullptr };
 		LeafNode* _previous{ nullptr };
 
-#undef LEAF
+
+
 	public:
 		LeafNode(shared_ptr<_LessThan> lessThan)
 			: Base(), _elements(/*right value passed to ref value EmptyEnumerator<pair<Key, Value>>(),*/ lessThan)
@@ -104,16 +122,16 @@ namespace Collections
 				}
 				else
 				{
-					auto preCount = _previous->_elements.Count();
-					auto nxtCount = _next->_elements.Count();
-					auto average = (preCount + this->_elements.Count() + nxtCount) / 3;
-					if (int(preCount - average) < int(nxtCount - average))
+					switch (ChooseOperatePosition<Operation::Add>(_previous->_elements.Count(),
+																  this->_elements.Count(),
+																  _next->_elements.Count()))
 					{
+					case Position::Previous:
 						addToPre = true;
-					}
-					else
-					{
+						break;
+					case Position::Next:
 						addToNxt = true;
+						break;
 					}
 				}
 			}
@@ -166,6 +184,7 @@ namespace Collections
 			constexpr order_int lowBound = 1 + ((BtreeOrder - 1) / 2);
 			if (_elements.Count() < lowBound)
 			{
+				bool nxtStealable = false, preStealable = false;
 				if (_next != nullptr)
 				{
 					if (_next->_elements.Count() == lowBound)
@@ -176,17 +195,18 @@ namespace Collections
 						_next->_elements.Add(move(items));
 						// Delete this
 						this->_upNodeDeleteSubNodeCallback(this);
+						return;
 					}
 					else
 					{
 						// Means next _elements bigger than lowBound
 						// only one which is lower than lowBound is root leaf which doesn't have siblings
 						// Or steal one, could think steal one from which sibling in more balance view
-						auto item = this->_next->_elements.FrontPopOut();
-						this->_elements.Append(move(item));
+						nxtStealable = true;
 					}
 				}
-				else if (_previous != nullptr)
+				
+				if (_previous != nullptr)
 				{
 					if (_previous->_elements.Count() == lowBound)
 					{
@@ -194,15 +214,55 @@ namespace Collections
 						_previous->_elements.Add(move(items));
 						//_previous->_elements.Add(CreateEnumerator(items./*r*/begin(), items./*r*/end())); why error
 						this->_upNodeDeleteSubNodeCallback(this);
+						return;
 					}
 					else
 					{
 						// Means next _elements bigger than lowBound
 						// Or steal one, could think steal one from which sibling in more balance view
-						auto items = this->_previous->_elements.PopOutItems(1);
-						this->_elements.Insert(move(items[0]));
+						preStealable = true;
 					}
 				}
+
+				if (nxtStealable && preStealable)
+				{
+					switch (ChooseOperatePosition<Operation::Remove>(_previous->_elements.Count(), this->_elements.Count(),
+						_next->_elements.Count()))
+					{
+					case Position::Next:
+						goto StealNxt;
+					case Position::Previous:
+						goto StealPre;
+					}
+				}
+				else if (nxtStealable)
+				{
+					goto StealNxt;
+
+				}
+				else if (preStealable)
+				{
+					goto StealPre;
+				}
+				else
+				{
+					goto NothingToDo;
+				}
+
+			StealNxt:
+				{
+					auto item = this->_next->_elements.FrontPopOut();
+					this->_elements.Append(move(item));
+					return;
+				}
+			StealPre:
+				{
+					auto items = this->_previous->_elements.PopOutItems(1);
+					this->_elements.Insert(move(items[0]));
+					return;
+				}
+			NothingToDo:
+				return;
 			}
 		}
 
@@ -247,6 +307,29 @@ namespace Collections
 			}
 
 			return _next->CollectKeys(move(previousNodesKeys));
+		}
+
+
+		template <Operation Op>
+		static Position ChooseOperatePosition(order_int preCount, order_int thisCount, order_int nxtCount)
+		{
+			if constexpr (Op == Operation::Add)
+			{
+				auto average = (preCount + thisCount + nxtCount) / 3;
+				if (int(preCount - average) < int(nxtCount - average))
+				{
+					return Position::Previous;
+				}
+				else
+				{
+					return Position::Next;
+				}
+			}
+			else
+			{
+				return ChooseOperatePosition<Operation::Add>(preCount, thisCount, nxtCount) == Position::Previous ?
+					Position::Next : Position::Previous;
+			}
 		}
 	};
 }
