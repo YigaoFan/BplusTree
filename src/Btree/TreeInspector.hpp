6eef38ec78044d6a40843fa2b45c3232270753e0
YigaoFan <fanyigao@qq.com>
2020-03-08 20:31:17 +0800

    fix indent

--- src/Btree/TreeInspector.hpp
+++ src/Btree/TreeInspector.hpp
@@ -8,13 +8,13 @@
 
 namespace Collections
 {
-    using ::std::cout;
-    using ::std::endl;
+	using ::std::cout;
+	using ::std::endl;
 
-    template <typename Key, typename Value, order_int BtreeOrder>
-    auto MoveDeep(vector<NodeBase<Key, Value, BtreeOrder>*> deepNodes) -> decltype(deepNodes)
-    {
-        auto deepers = decltype(deepNodes)();
+	template <typename Key, typename Value, order_int BtreeOrder>
+	auto MoveDeep(vector<NodeBase<Key, Value, BtreeOrder>*> deepNodes) -> decltype(deepNodes)
+	{
+		auto deepers = decltype(deepNodes)();
 		for (auto n : deepNodes)
 		{
 			for (auto sn : n->SubNodes())
@@ -23,28 +23,28 @@ namespace Collections
 			}
 		}
 
-        return deepers;
-    }
+		return deepers;
+	}
 
-    template <typename Key, typename Value, order_int BtreeOrder>
-    void InspectNodeKeys(NodeBase<Key, Value, BtreeOrder>* node)
-    {
-        auto deep = 0;
-        auto deepNodes = vector<decltype(node)>{ node, };
-        do
-        {
-            for (auto n : deepNodes)
-            {
-                for (auto& k : n->SubNodeMinKeys())
-                {
-                    cout << k << " ";
-                }
+	template <typename Key, typename Value, order_int BtreeOrder>
+	void InspectNodeKeys(NodeBase<Key, Value, BtreeOrder>* node)
+	{
+		auto deep = 0;
+		auto deepNodes = vector<decltype(node)>{ node, };
+		do
+		{
+			for (auto n : deepNodes)
+			{
+				for (auto& k : n->SubNodeMinKeys())
+				{
+					cout << k << " ";
+				}
 
-                cout << "  ";
-            }
+				cout << "  ";
+			}
 
-            cout << endl;
-            deepNodes = MoveDeep(deepNodes);
-        } while (deepNodes.size() != 0);
-    }
+			cout << endl;
+			deepNodes = MoveDeep(deepNodes);
+		} while (deepNodes.size() != 0);
+	}
 }
