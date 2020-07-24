3b86946fc72502c40fbea69dbe9a62e6e9305516
YigaoFan <fanyigao@qq.com>
2020-07-24 18:44:57 +0800

    fine DiskPos.hpp

--- src/FuncLib/DiskPos.hpp
+++ src/FuncLib/DiskPos.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include <map>
 #include <memory>
 #include <type_traits>
 #include "File.hpp"
@@ -10,7 +9,6 @@ namespace FuncLib
 	using ::std::is_abstract_v;
 	using ::std::is_base_of_v;
 	using ::std::make_shared;
-	using ::std::map;
 	using ::std::shared_ptr;
 	using ::std::size_t;
 
@@ -21,7 +19,6 @@ namespace FuncLib
 		friend struct ByteConverter<DiskPos, false>;
 		template <typename Ty>
 		friend class DiskPos;
-		static map<size_t, shared_ptr<T>> _cache; // �� start Ӧ����û�µ�, if change, should delete
 
 		shared_ptr<File> _file;
 		size_t _start;
@@ -31,29 +28,21 @@ namespace FuncLib
 		DiskPos(shared_ptr<File> file, size_t start) : _start(start), _file(file)
 		{ }
 
-		template <typename Derive, typename = enable_if_t<is_base_of_v<T, Derive>>>
+		template <typename Derive>
 		DiskPos(DiskPos<Derive> const& that) : _file(that._file), _start(that._start)
 		{ }
 
-		shared_ptr<T> ReadObject()
+		shared_ptr<T> ReadObject() const
 		{
 			if constexpr (is_abstract_v<T>)
 			{
-				return ByteConverter<T>::ConvertFromByte(_file, _start); // ConvertFromByte need to ensure return a shared_ptr
+				// ConvertFromByte need to ensure return a shared_ptr
+				return ByteConverter<T>::ConvertFromByte(_file, _start);
 			}
 			else
 			{
 				return make_shared<T>(ByteConverter<T>::ConvertFromByte(_file, _start));
 			}
-			
-			//if (!this->_cache.contains(_start))
-			//{
-			//	auto p = shared_ptr(CurrentFile::Read(_start, _size));
-			//	_cache.insert({ _start, p });
-			//	return p;
-			//}
-
-			//return this->_cache[_start];
 		}
 
 		void WriteObject(shared_ptr<T> entity)
@@ -66,7 +55,5 @@ namespace FuncLib
 		{
 			return _file;
 		}
-
-		// ~DiskPos(); will use?
 	};
 }
