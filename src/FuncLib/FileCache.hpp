4244a95a8dfe2469dcf854d5695fe684b7cebfd8
YigaoFan <fanyigao@qq.com>
2020-08-31 22:28:59 +0800

    update FileCache.hpp

--- src/FuncLib/FileCache.hpp
+++ src/FuncLib/FileCache.hpp
@@ -3,70 +3,77 @@
 #include <utility>
 #include <map>
 #include <functional>
+#include <filesystem>
+#include "../Basic/Exception.hpp"
 
 namespace FuncLib
 {
+	using ::Basic::InvalidAccessException;
 	using ::std::function;
 	using ::std::make_pair;
 	using ::std::map;
+	using ::std::move;
 	using ::std::pair;
 	using ::std::shared_ptr;
 	using ::std::vector;
+	using ::std::weak_ptr;
+	using ::std::filesystem::path;
 
-	class File;
-	
 	class FileCache
 	{
 	private:
 		template <typename T>
-		static map<shared_ptr<File>, map<size_t, pair<shared_ptr<T>, vector<function<void(T*)>>>>> Cache;
-		static map<shared_ptr<File>, function<void()>> Unloaders;
+		static map<path, map<size_t, weak_ptr<T>>> Cache;
 
+		path const& _filename;
+		function<void()> _unloader;
 	public:
-		FileCache() // shared_ptr<File> file
-		{
-			// Use this constructor to obtain a access key(token)
-		}
+		FileCache(path const& filename) : _filename(filename)
+		{ }
 
 		template <typename T>
 		bool HasRead(size_t offset)
 		{
-			shared_ptr<File> file;
-			// maybe should compare by File operator==
-			return Cache<T>.contains(file) && Cache<T>[file].contains(offset);
+			return Cache<T>.contains(_filename) && Cache<T>[_filename].contains(offset);
 		}
 
 		template <typename T>
-		shared_ptr<T> Add(size_t offset, shared_ptr<T> object)
+		void Add(size_t offset, shared_ptr<T> object)
 		{
-			shared_ptr<File> file;
-
-			Cache<T>[file][offset] = make_pair<shared_ptr<T>, vector<function<void(T*)>>>(object, { });
-
-			auto newUnloader = [&target = Cache<T>[file][offset]]()
+			if (!Cache<T>.contains(_filename))
 			{
-				for (auto& setter : target.second)
+				_unloader = [&file = _filename, &fileCache = Cache<T>, previousUnloader = move(_unloader)]()
 				{
-					setter(target.first.get());
-				}
-			};
-
-			if (Unloaders.contains(file))
-			{
-				auto preUnloader = Unloaders[file];
-				newUnloader = [= move(preUnloader), newPart = move(newUnloader)]()
-				{
-					preUnloader();
-					newPart();
+					previousUnloader();
+					fileCache.erase(file);
 				};
 			}
 
-			Unloaders[file] = move(newUnloader);
+			Cache<T>.insert(make_pair<path>(_filename, { offset, object }));
 		}
-		
+
+		template <typename T>
+		shared_ptr<T> Read(size_t offset)
+		{
+			// release version code should without check
+			auto& weakPtr = Cache<T>[_filename][offset];
+			if (weakPtr.expired())
+			{
+				throw InvalidAccessException
+					("read expired object in cache. please check code logic");
+			}
+			else
+			{
+				return weakPtr.lock();
+			}
+		}
+
 		~FileCache()
 		{
-			// Invoke all unloaders
+			_unloader();
 		}
-	}; 
+	};
+
+	template <typename T>
+	map<path, map<size_t, weak_ptr<T>>> FileCache::Cache = {};
 }
