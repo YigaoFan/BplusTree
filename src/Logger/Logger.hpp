3a4dbb896f47eef3f7c528c83642bf926ec7d8d8
YigaoFan <fanyigao@qq.com>
2020-09-07 18:55:34 +0800

    add Logger.hpp

---
+++ src/Logger/Logger.hpp
@@ -0,0 +1,19 @@
+namespace Log
+{
+	class Logger
+    {
+    private:
+        /* data */
+    public:
+        Logger(/* args */);
+        ~Logger();
+    };
+    
+    Logger::Logger(/* args */)
+    {
+    }
+    
+    Logger::~Logger()
+    {
+    }
+}
