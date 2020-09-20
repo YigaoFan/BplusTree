10a20f1ce67a130dd34135f7c7117df04c621112
YigaoFan <fanyigao@qq.com>
2020-09-20 19:16:00 +0800

    update StorageAllocator

--- src/FuncLib/Store/StorageAllocator.cpp
+++ src/FuncLib/Store/StorageAllocator.cpp
@@ -3,19 +3,32 @@
 
 namespace FuncLib::Store
 {
-	StorageAllocator::StorageAllocator(File *file)
+	StorageAllocator::StorageAllocator(File* file)
 		: _file(file), _currentPos(0), _ownerTable(ReadAllocatedInfoFrom(file))
 	{ }
 
-	void StorageAllocator::SignIn(shared_ptr<IInsidePositionOwner> positionOwner)
+	void StorageAllocator::SignIn(shared_ptr<InsidePositionOwner> positionOwner)
 	{
 		_ownerTable.push_back(positionOwner);
 	}
 
-	vector<shared_ptr<IInsidePositionOwner>> StorageAllocator::ReadAllocatedInfoFrom(File const *file)
+	pos_int StorageAllocator::Allocate(size_t size)
+	{
+		auto pos = _currentPos;
+		_currentPos += size;
+		return pos;
+	}
+
+	pos_int StorageAllocator::Reallocate(pos_int previousPosition, size_t newSize)
+	{
+		// remove previous position in allocated table
+		return Allocate(newSize);
+	}
+	vector<shared_ptr<InsidePositionOwner>> StorageAllocator::ReadAllocatedInfoFrom(File const* file)
 	{
 		// TODO
-		// use 
+		// SignIn 里的都在这个读到的列表里
+		auto path = file->Path();
 		return {};
 	}
 }
--- src/FuncLib/Store/StorageAllocator.hpp
+++ src/FuncLib/Store/StorageAllocator.hpp
@@ -3,13 +3,13 @@
 #include <vector>
 #include <filesystem>
 #include "StaticConfig.hpp"
-#include "IInsidePositionOwner.hpp"
+#include "InsidePositionOwner.hpp"
 
 namespace FuncLib::Store
 {
 	using ::std::shared_ptr;
 	using ::std::vector;
-
+	
 	class File;
 	// 分配信息需要保存到硬盘上
 	class StorageAllocator
@@ -17,22 +17,13 @@ namespace FuncLib::Store
 	private:
 		File* _file;
 		pos_int _currentPos;
-		vector<shared_ptr<IInsidePositionOwner>> _ownerTable;
+		vector<shared_ptr<InsidePositionOwner>> _ownerTable; // InsidePositionOwner 这个类应该是要让存储的位置信息的时候要用
 	public:
 		StorageAllocator(File* file);
-		void SignIn(shared_ptr<IInsidePositionOwner> positionOwner);
-
-		template <typename T>
-		shared_ptr<IInsidePositionOwner> Allocate()
-		{
-			auto pos = _currentPos;
-			_currentPos += sizeof(T);
-			auto owner = MakePositionOwner<T>(_file, pos);
-			_ownerTable.emplace_back(owner);
-			return owner;
-		}
-
+		void SignIn(shared_ptr<InsidePositionOwner> positionOwner);
+		pos_int Allocate(size_t size);
+		pos_int Reallocate(pos_int previousPosition, size_t newSize);
 	private:
-		static vector<shared_ptr<IInsidePositionOwner>> ReadAllocatedInfoFrom(File const* file);
+		static vector<shared_ptr<InsidePositionOwner>> ReadAllocatedInfoFrom(File const* file);
 	};
 }
