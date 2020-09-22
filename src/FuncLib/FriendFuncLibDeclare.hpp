d8bb886c1cc9191c8c6d03fac6e40c5989ce7456
YigaoFan <fanyigao@qq.com>
2020-09-22 22:16:31 +0800

    update type trait to omit ref

--- src/FuncLib/FriendFuncLibDeclare.hpp
+++ src/FuncLib/FriendFuncLibDeclare.hpp
@@ -3,12 +3,14 @@
 
 namespace FuncLib
 {
+	template <typename T>
+	using RR = ::std::remove_reference_t<T>;
 	using ::std::is_trivial_v;
 	using ::std::is_standard_layout_v;
 
-	template <typename T, bool = is_standard_layout_v<T> && is_trivial_v<T>>
+	template <typename T, bool = is_standard_layout_v<RR<T>> && is_trivial_v<RR<T>>>
 	struct ByteConverter;
-	template <typename T, bool = is_standard_layout_v<T> && is_trivial_v<T>>
+	template <typename T, bool = is_standard_layout_v<RR<T>> && is_trivial_v<RR<T>>>
 	struct TypeConverter;
 	// For PtrSetter
 	template <typename T>
