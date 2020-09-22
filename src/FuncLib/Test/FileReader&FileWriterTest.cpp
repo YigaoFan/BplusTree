32c4a806722014a56e5c470b5c4cea63adcb8788
YigaoFan <fanyigao@qq.com>
2020-09-22 21:23:04 +0800

    add FileReader&FileWriterTest.cpp

---
+++ src/FuncLib/Test/FileReader&FileWriterTest.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include "../../TestFrame/FlyTest.hpp"
+#include "../Store/FileReader.hpp"
+#include "../Store/FileWriter.hpp"
+
+using namespace FuncLib::Store;
+using namespace std;
+
+shared_ptr<path> MakeFilePath(string filename)
+{
+	return make_shared<path>(filename);
+}
+
+TESTCASE("FileReader&FileWriter test")
+{
+	auto filename = "reader&writer";
+	auto writer = FileWriter(MakeFilePath(filename), 0);
+	string s = "Hello World";
+	writer.Write(s.c_str(), s.size());
+	writer.Flush();
+	ASSERT(writer.CurrentPos() == s.size());
+	
+	auto reader = FileReader(MakeFilePath("reader&writer"), 0);
+	ASSERT(reader.GetLessOwnershipFile() == nullptr);
+	
+	auto bytes = reader.Read(s.size());
+	ASSERT(bytes.size() == s.size());
+	for (auto i = 0; i < s.size(); ++i)
+	{
+		ASSERT(s[i] == static_cast<char>(bytes[i]));
+	}
+
+	auto zeroBytes = reader.Read<0>();
+	static_assert(zeroBytes.size() == 0);
+
+	remove(filename);
+}
+
+DEF_TEST_FUNC(fileReader_FileWriterTest)
