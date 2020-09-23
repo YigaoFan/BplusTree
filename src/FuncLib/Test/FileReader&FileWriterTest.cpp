6ed8be99971f2a9ca7cef4a7ed22a461e85d4a61
YigaoFan <fanyigao@qq.com>
2020-09-23 16:44:04 +0800

    fix test code problem

--- src/FuncLib/Test/FileReader&FileWriterTest.cpp
+++ src/FuncLib/Test/FileReader&FileWriterTest.cpp
@@ -11,24 +11,26 @@ using namespace FuncLib::Test;
 TESTCASE("FileReader&FileWriter test")
 {
 	auto filename = "reader&writer";
-	auto writer = FileWriter(MakeFilePath(filename), 0);
-	string s = "Hello World";
-	writer.Write(s.c_str(), s.size());
-	writer.Flush();
-	ASSERT(writer.CurrentPos() == s.size());
-	
-	auto reader = FileReader(MakeFilePath("reader&writer"), 0);
-	ASSERT(reader.GetLessOwnershipFile() == nullptr);
-	
-	auto bytes = reader.Read(s.size());
-	ASSERT(bytes.size() == s.size());
-	for (auto i = 0; i < s.size(); ++i)
 	{
-		ASSERT(s[i] == static_cast<char>(bytes[i]));
-	}
+		auto writer = FileWriter(MakeFilePath(filename), 0);
+		string s = "Hello World";
+		writer.Write(s.c_str(), s.size());
+		writer.Flush();
+		ASSERT(writer.CurrentPos() == s.size());
 
-	auto zeroBytes = reader.Read<0>();
-	static_assert(zeroBytes.size() == 0);
+		auto reader = FileReader(MakeFilePath("reader&writer"), 0);
+		ASSERT(reader.GetLessOwnershipFile() == nullptr);
+
+		auto bytes = reader.Read(s.size());
+		ASSERT(bytes.size() == s.size());
+		for (auto i = 0; i < s.size(); ++i)
+		{
+			ASSERT(s[i] == static_cast<char>(bytes[i]));
+		}
+
+		auto zeroBytes = reader.Read<0>();
+		static_assert(zeroBytes.size() == 0);
+	}// 提前让 writer 析构去 flush 到文件，以免下面 remove 之后又 flush
 
 	remove(filename);
 }
