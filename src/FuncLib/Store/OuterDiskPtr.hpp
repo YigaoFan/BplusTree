c9e34fd1b8d3f3926876c69619089c6131e020ae
YigaoFan <fanyigao@qq.com>
2020-10-21 20:55:17 +0800

    update OuterDiskPtr method

--- src/FuncLib/Store/OuterDiskPtr.hpp
+++ src/FuncLib/Store/OuterDiskPtr.hpp
@@ -1,6 +1,5 @@
 #include <memory>
 #include "File.hpp"
-#include "../ByteConverter.hpp"
 
 namespace FuncLib::Store
 {
@@ -15,9 +14,48 @@ namespace FuncLib::Store
 		
 	public:
 		OuterDiskPtr();
+
+		void Delete()
+		{
+
+		}
+
+		// 因为 Store 本身带有保存 pos_lable 不释放的能力，所以不能交给用户来控制
+		~OuterDiskPtr()
+		{
+			WriteObject();
+		}
+
+	private:
 		void WriteObject()
 		{
 			_pos.WriteObject(_object);
 		}
+
+		// 这个功能可能不要了，可能放在 OuterPointer 那里实现了
+		// void File::Flush()
+		// {
+		// 	// Flush 有点类似这整个析构一次的效果
+		// 	for (auto& cacheKit : _cache)
+		// 	{
+		// 		get<1>(cacheKit)();
+		// 	}
+		// }
+
+		/// caller should ensure wake all root element, j
+		/// ust like a btree can wake all inner elements, but not other btree.
+		// 放在 OuterPointer 那里实现
+		// 存储位置的事就全由 OuterPointer 来负责了
+		// void File::ReallocateContent()
+		// {
+		// 	// 还需要想很多，比如这一步很可能是需要其他步骤一起做才有效的
+		// 	auto addr = 0; // reallocate from 0
+		// 	for (auto& cacheKit : _cache)
+		// 	{
+		// 		shared_ptr<InsidePositionOwner> posOwner = get<3>(cacheKit);
+		// 		posOwner->Addr(addr);
+		// 		// addr += posOwner->RequiredSize();
+		// 	}
+		// }
 	};
 }
