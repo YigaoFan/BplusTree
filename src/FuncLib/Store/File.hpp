55e30d56d6b52c496a2f2c804962908a037bb256
YigaoFan <fanyigao@qq.com>
2020-11-04 21:52:21 +0800

    add important design thought comment

--- src/FuncLib/Store/File.hpp
+++ src/FuncLib/Store/File.hpp
@@ -23,6 +23,38 @@ namespace FuncLib::Store
 	using ::std::shared_ptr;
 	using ::std::filesystem::path;
 
+	template <typename... Ts>
+	struct TypeList;
+
+	template <typename T>
+	struct TypeList<T>
+	{
+		static constexpr bool IsLast = true;
+		using Current = T;
+	};
+
+	template <typename T, typename... Ts>
+	struct TypeList<T, Ts...>
+	{
+		static constexpr bool IsLast = false;
+		using Current = T;
+		using Remain = TypeList<Ts...>;
+	};
+
+	template <template <typename> class Handler, typename... Ts>
+	auto ForEach(TypeList<Ts...> typelist)
+	{
+		using T = Handler<typename decltype(typelist)::Current>;
+		// if (handler(T)) return type should here
+
+		if constexpr (not typelist.IsLast)
+		{
+			ForEach<Handler>(typelist.Remain());
+		}
+	}
+
+	// template <typename T>
+	// using SearchRoutine = TypeList<NodeBase<T>, MiddleNode<T>, LeafNode<T>>;
 	/// 一个路径仅有一个 File 对象，这里的功能大部分是提供给模块外部使用的
 	/// 那这里就有点问题了，Btree 内部用这个是什么功能
 	/// 这样使用场景是不是就不干净了？指的是内外都用。
@@ -39,7 +71,8 @@ namespace FuncLib::Store
 		ObjectRelationTree _objRelationTree;
 	public:
 		static shared_ptr<File> GetFile(path const& filename);
-		File(unsigned int fileId, path filename); // for make_shared use in File class only
+		/// below for make_shared use in File class only
+		File(unsigned int fileId, path filename, StorageAllocator allocator, ObjectRelationTree relationTree);
 		File(File&& that) noexcept = delete;
 		File(File const& that) = delete;
 
@@ -58,6 +91,7 @@ namespace FuncLib::Store
 			// {
 			// 	Read MiddleNode
 			// }
+			// New 的时候要用本来的类型，而不要用动态类型
 			
 			if (_cache.Cached<T>(posLable))
 			{
@@ -91,83 +125,45 @@ namespace FuncLib::Store
 		{
 			_toDeallocateLables.erase(posLable);
 
-			// 这里的 SizeStable 要不要考虑指针指向的对象呀，牵涉到 ByteConverter<DiskPtr> 和这下面的 if
+			// 这里的 SizeStable 不考虑指针指向的对象，牵涉到 ByteConverter<DiskPtr> 和这下面的 if
 
-			// 触发 写 的唯一一个地方
-			auto bytes = ObjectBytes(posLable);
-			pos_int start;
-
-			if (_allocator.Ready(posLable))
+			WriteQueue wq;
+			AllocateSpaceQueue aq;
+			ResizeSpaceQueue rq;
+			ObjectBytes bytes{ posLable, &wq, &aq, &rq };
+			ProcessStore(posLable, object, &bytes);
+			auto ready = false;
+			if (not ready)
 			{
-				start = _allocator.GetConcretePos(posLable);
-				if constexpr (ByteConverter<T>::SizeStable)
-				{
-					ByteConverter<T>::WriteDown(*object, &bytes);
-				}
-				else
-				{
-					ByteConverter<T>::WriteDown(*object, &bytes);
-					auto previousSize = _allocator.GetAllocatedSize(posLable);
-					auto newSize = bytes.Size();
-					if (previousSize < newSize)
-					{
-						start = _allocator.ResizeSpaceTo(posLable, newSize);
-					}
-				}
+				// 说明第一次 Store，可以所有层次的要分配的空间大小一起分配，获取 vector<pair<pos_lable, size_t>> 来让
+				// allocator 分配
+				// 这里的从
 			}
 			else
 			{
-				ByteConverter<T>::WriteDown(*object, &bytes);
-				auto size = bytes.Size();
-				start = _allocator.GiveSpaceTo(posLable, size);
+				// 说明至少第二次 Store，各个已经拥有独立的地址和空间（独立性保证没读取过的内容的安全）
+				// 这时各层次可以各分各的
 			}
-
-			bytes.WriteIn(*_filename, start);
-
-			// update object relation
-			_objRelationTree.UpdateWith(&bytes);
-
-			// 可能需要 assert 这里的 start 和 writer 的当前地址要一样，有的情况下可能不一样也是对的
-			// 要基于位置都是偏移的抽象的基础去工作，感觉有点复杂了可能，之后再想
+			
+			auto pos = 0;// TODO remove
+			bytes.WriteIn(*_filename, pos);
+			_objRelationTree.UpdateWith(&bytes, [](pos_lable) {});
 		}
 
+		// 内外在一起写，是不是就代表内部不用获取具体的位置？
+		// 如果不在一起写，pos_int 在 writer 里的变化机制要变
+		// 还有没有读的部分，在写入的时候要怎么处理
+		// inner 如果大小固定可以和上层放在一块，不固定就可以隔离出去
+
 		template <typename T>
-		void Store(pos_lable posLable, shared_ptr<T> const& object, IWriter auto* parentWriter)
+		void StoreInner(pos_lable posLable, shared_ptr<T> const& object, IWriter auto* parentWriter)
 		{
-			_toDeallocateLables.erase(posLable);
+			_toDeallocateLables.erase(posLable);// 这个操作在有 inner 的情况下对不对 TODO
 
-			using Writer = std::remove_pointer_t<decltype(parentWriter)>;
-			auto bytes = new Writer(posLable);
+			auto parent = parentWriter;
+			auto bytes = new ObjectBytes(posLable, parent->ToWrites, parent->ToAllocates, parent->ToResize);
 			parentWriter->AddSub(bytes);
-			pos_int start;
-
-			// 触发 写 的唯一一个地方
-			if (_allocator.Ready(posLable))
-			{
-				start = _allocator.GetConcretePos(posLable);
-				if constexpr (ByteConverter<T>::SizeStable)
-				{
-					ByteConverter<T>::WriteDown(*object, &bytes);
-				}
-				else
-				{
-					ByteConverter<T>::WriteDown(*object, &bytes);
-					auto previousSize = _allocator.GetAllocatedSize(posLable);
-					auto newSize = bytes.Size();
-					if (previousSize < newSize)
-					{
-						start = _allocator.ResizeSpaceTo(posLable, newSize);
-					}
-				}
-			}
-			else
-			{
-				ByteConverter<T>::WriteDown(*object, &bytes);
-				auto size = bytes.Size();
-				start = _allocator.GiveSpaceTo(posLable, size);
-			}
-
-			bytes.WriteIn(*_filename, start);
+			ProcessStore(posLable, object, bytes);
 		}
 
 		template <typename T>
@@ -175,10 +171,14 @@ namespace FuncLib::Store
 		{
 			// delete related sub pos_lable TODO
 			// also related to toBeDeleteLables
-			_toDeallocateLables.erase(posLable);
+			// Store 那里同样要做这个工作
+			_objRelationTree.DeleteTopLevelObj(posLable, [&](pos_lable lable)
+			{
+				_toDeallocateLables.erase(lable);
+				_allocator.DeallocatePosLable(lable);
+			});
 
 			_cache.Remove<T>(posLable);
-			_allocator.DeallocatePosLable(posLable);
 		}
 
 	private:
@@ -207,6 +207,44 @@ namespace FuncLib::Store
 			_toDeallocateLables.insert(posLable);
 			_cache.Add<T>(posLable, obj);
 			return obj;
+		}
+
+		template <typename T>
+		void ProcessStore(pos_lable posLable, shared_ptr<T> const& object, ObjectBytes* bytes)
+		{
+			// auto bytes = new ObjectBytes(posLable, parentWriter->ToWrites, parentWriter->ToAllocates);
+			// pos_int start;
+
+			ByteConverter<T>::WriteDown(*object, bytes);// 这里把 bytes 准备好，这里的 bytes 都是和地址无关的
+
+			// 触发 写 的唯一一个地方
+			if (_allocator.Ready(posLable))
+			{
+				// start = _allocator.GetConcretePos(posLable);
+				if constexpr (not ByteConverter<T>::SizeStable)
+				{
+					auto previousSize = _allocator.GetAllocatedSize(posLable);
+					auto newSize = bytes->Size();
+					if (previousSize < newSize)
+					{
+						// 加入重分配区
+						// start = _allocator.ResizeSpaceTo(posLable, newSize);
+						bytes->ToResizes->Add(bytes);
+						return;
+					}
+				}
+
+				// 加入待写区
+				bytes->ToWrites->Add(bytes);
+			}
+			else
+			{
+				// 加入待分配区
+				bytes->ToAllocates->Add(bytes);
+
+				// auto size = bytes.Size();
+				// start = _allocator.GiveSpaceTo(posLable, size);
+			}
 		}
 	};
 }
