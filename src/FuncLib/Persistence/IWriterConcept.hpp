ddd43a6800b92b715bd583781032ac6ed37af4d5
YigaoFan <fanyigao@qq.com>
2020-11-12 23:25:36 +0800

    1. delete unused file 2. add comment

--- src/FuncLib/Persistence/IWriterConcept.hpp
+++ src/FuncLib/Persistence/IWriterConcept.hpp
@@ -25,6 +25,7 @@ namespace FuncLib::Persistence
 	// 	{ t.Size() } -> IsSameTo<size_t>;
 	// };
 
+	/// 在 ByteConverter 这里面用 IWriter，在外面可以不用，就像 ObjectBytes 和 FakeWriter 在 File 里那样
 	template <typename T>
 	concept IWriter = Writer_Write<T> and 
 					  Writer_WriteBlank<T>;
--- src/FuncLib/PersistenceData.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace FuncLib
-{
-	// For Btree store on disk
-}
