0967653b37642b8d5af03a5aeb56c7e511fbfbf7
YigaoFan <fanyigao@qq.com>
2020-11-13 17:14:12 +0800

    delete unused files

--- src/FuncLib/Generator.hpp
+++
@@ -1,8 +0,0 @@
-// Created by 范义高 on 2019-03-03.
-#pragma once
-// this file and Generator.cpp should be move into another project
-// called function_lib
-
-namespace func_lib {
-	void generate_function_library(); // should set some args maybe
-}
