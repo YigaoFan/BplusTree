16a21a0f501c03ea415c9f05ea4ee744b058ca2f
YigaoFan <fanyigao@qq.com>
2020-11-30 09:34:15 +0800

    update OuterPtr

--- src/FuncLib/Store/OuterPtr.hpp
+++ src/FuncLib/Store/OuterPtr.hpp
@@ -5,23 +5,31 @@ namespace FuncLib::Store
 {
 	using ::std::shared_ptr;
 
+	// 这个类的作用和 DiskPtr 是不一样的，这个类是对读取出来的 object 的封装
 	// 内外两种是不是可以自由的转换
 	template <typename T>
-	class OuterDiskPtr : public DiskPtrBase
+	class OuterPtr
 	{
 	private:
-		shared_ptr<File> _file;// 这个应该没有什么作用，只是掌握 File 的生命期用的
+		shared_ptr<T> _object;
+		shared_ptr<File> _file;// 这个有作用的，对外使用的时候可以隐藏 File，并且拥有 File 的生命期用
 		
 	public:
-		OuterDiskPtr();
+		OuterPtr(OuterPtr const&) = delete;
 
-		void Delete()
+		OuterPtr(OuterPtr&& that) noexcept
 		{
 
 		}
 
-		// 因为 Store 本身带有保存 pos_lable 不释放的能力，所以不能交给用户来控制
-		~OuterDiskPtr()
+		void Delete()
+		{
+			// 没读 delete 会有问题，各个子部分都会出现这种情况
+			// 可以构造个包含关系的树
+		}
+
+		// 因为 Store 本身带有保存 pos_label 不释放的能力，所以不能交给用户来控制
+		~OuterPtr()
 		{
 			WriteObject();
 		}
@@ -31,16 +39,6 @@ namespace FuncLib::Store
 		{
 			_pos.WriteObject(_object);
 		}
-
-		// 这个功能可能不要了，可能放在 OuterPointer 那里实现了
-		// void File::Flush()
-		// {
-		// 	// Flush 有点类似这整个析构一次的效果
-		// 	for (auto& cacheKit : _cache)
-		// 	{
-		// 		get<1>(cacheKit)();
-		// 	}
-		// }
 
 		/// caller should ensure wake all root element, j
 		/// ust like a btree can wake all inner elements, but not other btree.
