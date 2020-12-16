4a89fc90861dec06c672865cf194c28383c16cca
YigaoFan <fanyigao@qq.com>
2020-12-16 11:21:09 +0800

    complete ThreadPool

--- src/Server/ThreadPool.hpp
+++ src/Server/ThreadPool.hpp
@@ -1,32 +1,90 @@
 #pragma once
+#include <queue>
+#include <mutex>
+#include <memory>
 #include <thread>
+#include <functional>
+#include <condition_variable>
 
 namespace Server
 {
+	using ::std::condition_variable;
+	using ::std::function;
+	using ::std::lock_guard;
+	using ::std::make_shared;
+	using ::std::move;
+	using ::std::mutex;
+	using ::std::queue;
+	using ::std::shared_ptr;
 	using ::std::thread;
+	using ::std::unique_lock;
 
+	// Code from: https://www.zhihu.com/question/27908489/answer/355105668
 	class ThreadPool
 	{
 	private:
-		/* data */
+		struct Data
+		{
+			mutex Mutex;
+			condition_variable CondVar;
+			bool IsShutdown = false;
+			queue<function<void()>> Tasks;
+		};
+
+		shared_ptr<Data> _data;
 	public:
-		ThreadPool(/* args */);
+		ThreadPool(size_t threadCount) : _data(make_shared<Data>())
+		{
+			auto n = thread::hardware_concurrency();
+			for (size_t i = 0; i < n; ++i)
+			{
+				thread([data = _data]
+				{
+					unique_lock<mutex> lock(data->Mutex);
+					for (;;)
+					{
+						if (not data->Tasks.empty())
+						{
+							auto current = move(data->Tasks.front());
+							data->Tasks.pop();
+							lock.unlock();
+							current();
+							lock.lock();
+						}
+						else if (data->IsShutdown)
+						{
+							break;
+						}
+						else
+						{
+							data->CondVar.wait(lock);
+						}
+					}
+				}).detach();
+			}
+		}
+
+		ThreadPool(ThreadPool&& that) noexcept = default;
 
 		template <typename Task>
 		void Execute(Task&& task)
 		{
+			{
+				lock_guard<mutex> lock(_data->Mutex);
+				_data->Tasks.emplace(std::forward<Task>(task));
+			}
 
+			_data->CondVar.notify_one();
 		}
 		
-		~ThreadPool();
+		~ThreadPool()
+		{
+			{
+				lock_guard<mutex> lock(_data->Mutex);
+				_data->IsShutdown = true;
+			}
+
+			_data->CondVar.notify_all();
+		}
 	};
-	
-	ThreadPool::ThreadPool(/* args */)
-	{
-	}
-	
-	ThreadPool::~ThreadPool()
-	{
-	}
-	
 }
