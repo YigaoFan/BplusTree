3db90ac21e8454b1d45b0d9f6fa96bcc35590f99
YigaoFan <fanyigao@qq.com>
2020-12-21 11:37:13 +0800

    add support to object method

--- src/Server/CurryLogger.hpp
+++ src/Server/CurryLogger.hpp
@@ -4,21 +4,36 @@ namespace Server
 {
 	using Basic::FuncTraits;
 	using ::std::forward;
+	using ::std::is_member_function_pointer_v;
 	using ::std::is_reference_v;
 	using ::std::move;
 
-	template <size_t CurrentArgIdx, typename FuncInfo, typename Func, typename... PreviousArgs>
+	// Object is pointer
+	template <typename Func, typename Object, typename... Args>
+	auto CallObjectMethod(Func func, Object object, Args... args)
+	{
+		return (object->*func)(forward<Args>(args)...);
+	}
+
+	template <size_t CurrentArgIdx, bool IsObjectMethod, typename FuncInfo, typename Func, typename... PreviousArgs>
 	auto DoCurry(Func func, PreviousArgs&&... previousArgs)
 	{
 		if constexpr (CurrentArgIdx == FuncInfo::ArgCount)
 		{
-			return func(forward<PreviousArgs>(previousArgs)...);
+			if constexpr (IsObjectMethod)
+			{
+				return CallObjectMethod(move(func), forward<PreviousArgs>(previousArgs)...);
+			}
+			else
+			{
+				return func(forward<PreviousArgs>(previousArgs)...);
+			}			
 		}
 		else if constexpr (CurrentArgIdx < FuncInfo::ArgCount)
 		{
 			return [move(func), ... previousArgs = forward<PreviousArgs>(previousArgs)]<typename Arg>(Arg&& arg) mutable
 			{
-				return DoCurry<CurrentArgIdx + 1, FuncInfo>(move(func), forward<PreviousArgs>(previousArgs)..., forward<Arg>(arg));
+				return DoCurry<CurrentArgIdx + 1, IsObjectMethod, FuncInfo>(move(func), forward<PreviousArgs>(previousArgs)..., forward<Arg>(arg));
 			};
 		}
 	}
@@ -26,6 +41,7 @@ namespace Server
 	template <typename Func>
 	auto Curry(Func func)
 	{
-		return DoCurry<0, FuncTraits<Func>>(func);
+		constexpr bool isObjMethod = is_member_function_pointer_v<Func>;
+		return DoCurry<0, isObjMethod, FuncTraits<Func>>(func);
 	}
 }
