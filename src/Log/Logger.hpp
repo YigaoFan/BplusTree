93c3ddf4d08ad403568068a4a9cbe952578d01ed
YigaoFan <fanyigao@qq.com>
2020-12-23 09:26:59 +0800

    simplify SubLogger code

--- src/Log/Logger.hpp
+++ src/Log/Logger.hpp
@@ -98,92 +98,69 @@ namespace Log
 	};
 
 	template <size_t BasicInfoLimitCount, typename CurryedAccessLogger, typename CurrentBasicInfoTuple>
-	struct Logger::SubLogger
+	struct Logger::SubLogger : private NonAccessLogSubLogger<CurrentBasicInfoTuple>
 	{
+		using Base = NonAccessLogSubLogger<CurrentBasicInfoTuple>;
 		CurryedAccessLogger CurryedAccessLog;
-		Logger* ParentLogger;
-		CurrentBasicInfoTuple BasicInfo; // 作为后来 log 的前缀
 
 		// 这里的构造函数推导要怎么用？可以直接用吗而不完全指定类型的模板参数，可见当时委员会的人也可能比较困惑
 		// 所以后来才支持这个特性
-		// template <typename... Infos>
 		SubLogger(CurryedAccessLogger curryedAccessLog, Logger *parentLogger, CurrentBasicInfoTuple infos)
-			: CurryedAccessLog(move(curryedAccessLog)), ParentLogger(parentLogger), BasicInfo(move(infos))
+			: Base(move(infos), parentLogger), CurryedAccessLog(move(curryedAccessLog))
 		{ }
 		// move constructor and set ParentLogger to nullptr
 
 		using NextInfo = typename FuncTraits<decltype(&CurryedAccessLogger::operator())>::template Arg<0>::Type;
 		auto BornNewWith(NextInfo info)
 		{
+			auto parent = this->ParentLogger;
+
 			if constexpr (not is_same_v<void, decltype(CurryedAccessLog(forward<NextInfo>(info)))>)
 			{
 				auto newerAccessLog = CurryedAccessLog(forward<NextInfo>(info));
-				auto parent = ParentLogger;
-				ParentLogger = nullptr;
 
-				if constexpr (tuple_size_v<decltype(BasicInfo)> < BasicInfoLimitCount)
+				if constexpr (tuple_size_v<decltype(this->BasicInfo)> < BasicInfoLimitCount)
 				{
-					auto newBasicInfo = tuple_cat(BasicInfo, tuple(remove_reference_t<NextInfo>(info)));
+					auto newBasicInfo = tuple_cat(this->BasicInfo, tuple(remove_reference_t<NextInfo>(info)));
 					return SubLogger<BasicInfoLimitCount, decltype(newerAccessLog), decltype(newBasicInfo)>{
 						move(newerAccessLog), parent, move(newBasicInfo) };
 				}
 				else
 				{
 					return SubLogger<BasicInfoLimitCount, decltype(newerAccessLog), CurrentBasicInfoTuple>{
-						move(newerAccessLog), parent, move(BasicInfo) };
+						move(newerAccessLog), parent, move(this->BasicInfo) };
 				}
 			}
 			else
 			{
 				CurryedAccessLog(forward<NextInfo>(info));
 
-				if constexpr (tuple_size_v<decltype(BasicInfo)> < BasicInfoLimitCount)
+				if constexpr (tuple_size_v<decltype(this->BasicInfo)> < BasicInfoLimitCount)
 				{
-					auto newBasicInfo = tuple_cat(BasicInfo, tuple(remove_reference_t<NextInfo>(info)));
-					return NonAccessLogSubLogger{ move(newBasicInfo) };
+					auto newBasicInfo = tuple_cat(this->BasicInfo, tuple(remove_reference_t<NextInfo>(info)));
+					return NonAccessLogSubLogger(move(newBasicInfo), parent);
 				}
 				else
 				{
-					return NonAccessLogSubLogger{ move(BasicInfo) };
+					return NonAccessLogSubLogger(move(this->BasicInfo), parent);
 				}
 			}
 		}
 
-		void Info(string message)
-		{
-
-		}
-
-		void Warn(string message)
-		{
-
-		}
-
-		template <typename Exception>
-		void Warn(string message, Exception const &exception)
-		{
-
-		}
-
-		void Error(string message)
-		{
-
-		}
-
-		template <typename Exception>
-		void Error(string message, Exception const &exception)
-		{
-
-		}
+		using Base::Info;
+		using Base::Warn;
+		using Base::Error;
 	};
 
 	template <typename CurrentBasicInfoTuple>
 	struct Logger::NonAccessLogSubLogger
 	{
 		CurrentBasicInfoTuple BasicInfo; // 作为后来 log 的前缀
+		Logger* ParentLogger;
 
 		// 原来构造函数的类模板参数推导可以这样用，结合调用的地方一起看
-		NonAccessLogSubLogger(CurrentBasicInfoTuple infos) : BasicInfo(move(infos))
+		NonAccessLogSubLogger(CurrentBasicInfoTuple infos, Logger* parentLogger)
+			: BasicInfo(move(infos)), ParentLogger(parentLogger)
 		{ }
 		
 		void Info(string message)
