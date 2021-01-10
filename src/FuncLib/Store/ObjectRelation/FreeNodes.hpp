a36d0eff2954a40fa3cbe592805c9ae2218700a0
YigaoFan <fanyigao@qq.com>
2021-01-10 21:40:29 +0800

    delete comment code

--- src/FuncLib/Store/ObjectRelation/FreeNodes.hpp
+++ src/FuncLib/Store/ObjectRelation/FreeNodes.hpp
@@ -16,14 +16,5 @@ namespace FuncLib::Store::ObjectRelation
 		{
 			_fileRoot.ReleaseAll(releaser);
 		}
-		
-		// FreeNodes(FreeNodes const& that) = delete;
-		// FreeNodes(FreeNodes&& that) noexcept = default;
-		// 有了下面这个函数，加上上面不声明那两个 default 和 delete，就会报使用了 vector 的移动构造函数的错（Node 不能复制构造）
-		// 可能是下面这个定义了就触发了编译器合成复制构造函数了？
-		// ~FreeNodes()
-		// {
-
-		// }
 	};	
 }
