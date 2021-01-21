d62a3f73bc6265905bf8f4cf4700f8688e35350c
YigaoFan <fanyigao@qq.com>
2021-01-21 16:25:03 +0800

    add Unity for dynamic cpp file compile

---
+++ src/FuncLib/Compile/Unity.hpp
@@ -0,0 +1,8 @@
+// For dynamic link cpp file pass compile
+#include <tuple>
+#include "Json/Json.cpp"
+#include "Basic/Debug.cpp"
+#include "Basic/Exception.cpp"
+#include "Json/JsonConverter/JsonConverter.cpp"
+#include "Json/ParseException.cpp"
+#include "Json/LocationInfo.cpp"
