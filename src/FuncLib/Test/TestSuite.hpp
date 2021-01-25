9c08fbc379be42c72a4d63fbb6824c5918dd4b52
YigaoFan <fanyigao@qq.com>
2021-01-25 21:56:29 +0800

    update FuncLib TestSuite

--- src/FuncLib/Test/TestSuite.hpp
+++ src/FuncLib/Test/TestSuite.hpp
@@ -1,29 +1,29 @@
 #pragma once
 extern void TestFile(bool executed);
 extern void TestFileReaderObjectBytes(bool executed);
-extern void byteConverterTest(bool executed);
 extern void TestStorageAllocator(bool executed);
 extern void TestFileCache(bool executed);
-extern void typeConverterTest(bool executed);
-extern void compileTest(bool executed);
+extern void TestCompile(bool executed);
 extern void TestLabelNode(bool executed);
 extern void TestObjectRelationTree(bool executed);
+extern void typeConverterTest(bool executed);
+extern void byteConverterTest(bool executed);
+extern void TestFunctionLibrary(bool executed);
 
 namespace FuncLib::Test
 {
 	void AllTest(bool executed)
 	{
-		if (false)
-		{
-			TestLabelNode(executed);
-			TestObjectRelationTree(executed);
-			TestStorageAllocator(executed);
-			TestFileReaderObjectBytes(executed);
-			TestFileCache(executed);
-		}
+		TestLabelNode(executed);
+		TestObjectRelationTree(executed);
+		TestFileReaderObjectBytes(executed);
+		TestFileCache(executed);
+		TestCompile(executed);
+		TestStorageAllocator(executed);
 		TestFile(executed);
+		TestFunctionLibrary(executed);
+		// 有时间可以整理下面这两个
 		typeConverterTest(false);
 		byteConverterTest(false);
-		compileTest(false);
 	}
 }
